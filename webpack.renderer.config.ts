03b7c21ad0757825a6008cfb1a3cad4fb6c73bf9
minhnhat2048 <nhatle2048@gmail.com>
2024-08-22 08:11:27 +0700

    build: add tailwind through postcss

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -5,7 +5,9 @@ import { plugins } from './webpack.plugins';
 
 rules.push({
   test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' },
+    //load tailwind
+    { loader: 'postcss-loader' }],
 });
 
 export const rendererConfig: Configuration = {
