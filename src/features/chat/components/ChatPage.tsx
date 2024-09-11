db9b86485cdd438fa97cf798998986d69dd6f4c0
minhnhat2048 <nhatle2048@gmail.com>
2024-09-11 07:59:01 +0700

    feat: add zustand

--- src/features/chat/components/ChatPage.tsx
+++ src/features/chat/components/ChatPage.tsx
@@ -1,13 +1,14 @@
 import { Button } from "../../../shared/components/shadcn-ui/button";
 import { ArrowLeftIcon, TriangleLeftIcon } from "@radix-ui/react-icons";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import ConversationList from "./conversation/ConversationList";
 
 const ChatPage = () => {
+  const navigate = useNavigate();
   return (
     <div className="h-screen w-full flex flex-col ">
       <div className="border-b border-gray-200">
-        <Button variant="link">
+        <Button variant="link" onClick={() => navigate(-1)}>
           <TriangleLeftIcon className="w-4 h-4" /> Back
         </Button>
       </div>
---
+++ src/shared/libs/zustand/auth.zustand.ts
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+import { EndUser } from "../../types/enduser.type";
+interface AuthStore {
+  endUser: EndUser;
+  setEndUser: (endUser: EndUser) => void;
+}
+
+const useAuthStore = create<AuthStore>()(
+  devtools((set) => ({
+    endUser: {
+      _id: "",
+      username: "",
+      email: "",
+      password: "",
+      avatar: "",
+      description: "",
+    },
+    setEndUser: (endUser) => set({ endUser }),
+  }))
+);
+
+export { useAuthStore };
