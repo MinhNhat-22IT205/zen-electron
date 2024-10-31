6cb8f7b48fed7c97c4bdfbdff6f539d868a1b7dd
minhnhat2048 <nhatle2048@gmail.com>
2024-10-31 21:14:19 +0700

    refactor: adjust livestream hooks

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,14 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { contextBridge, ipcRenderer } from "electron";
+
+declare global {
+  interface Window {
+    electron: {
+      getSources: () => Promise<any>;
+    };
+  }
+}
+contextBridge.exposeInMainWorld("electron", {
+  getSources: () => ipcRenderer.invoke("GET_SCREEN_SHARE_SOURCES"),
+});
